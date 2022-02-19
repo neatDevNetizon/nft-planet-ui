e572a0f772b2d3b77711ca1c301ebdabe21530a0
unknown <stf1388@gmail.com>
2022-02-19 19:22:43 +0300

    btn update

--- src/components/btn.jsx
+++ src/components/btn.jsx
@@ -1,5 +1,5 @@
 import React        from 'react';
-import { Button }   from '@chakra-ui/react';
+import { Button, Center }   from '@chakra-ui/react';
 import Txt          from "./txt";
 
 export const Btn = React.forwardRef((props, ref) => {
